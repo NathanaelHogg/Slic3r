8e26d963560567b23ac646083e1db69f562d682d
NathanaelHogg <164555230+NathanaelHogg@users.noreply.github.com>
2024-04-08 16:36:45 +0100

    Update Point.hpp

--- xs/src/libslic3r/Point.hpp
+++ xs/src/libslic3r/Point.hpp
@@ -34,8 +34,8 @@ class Point
     coord_t y;
     coord_t z;
     coord_t b;
-    Point(coord_t _x = 0, coord_t _y = 0, coord_t _z = -1, coord_t _z = = 0): x(_x), y(_y), z(_z), b(_b) {};
-    Point(int _x, int _y, int _z int _b): x(_x), y(_y), z(_z), b(_b) {};
+    Point(coord_t _x = 0, coord_t _y = 0, coord_t _z = -1, coord_t _b = 0): x(_x), y(_y), z(_z), b(_b) {};
+    Point(int _x, int _y, int _z, int _b): x(_x), y(_y), z(_z), b(_b) {};
     Point(long long _x, long long _y, long long _z, long long_b): x(_x), y(_y), z(_z), b(_b) {};  // for Clipper
     Point(double x, double y, double z, double b);
     static Point new_scale(coordf_t x, coordf_t y) {
