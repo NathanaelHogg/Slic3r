5c27a11302576d816535e977082f03891c8b5e0f
NathanaelHogg <164555230+NathanaelHogg@users.noreply.github.com>
2024-04-08 16:17:10 +0100

    Update LayerRegion.cpp

--- xs/src/libslic3r/LayerRegion.cpp
+++ xs/src/libslic3r/LayerRegion.cpp
@@ -364,6 +364,7 @@ struct old_point {
     double x;
     double y;
     double z;
+    double b;
 };
 
 void
@@ -480,10 +481,10 @@ LayerRegion::correct_z_on_path(ExtrusionPath *path)
             double dx = (point.x - point1.x);
             double dz = (point.z - point1.z);       
                 
-            b = std::atan(dz/dx);
+            double b = std::atan(dz/dx);
                 
-            nozzle_offset_from_b0_z = four_axis_Z_offset*(1- std::cos(b))
-            nozzle_offset_from_b0_x = four_axis_Z_offset* std::sin(b)
+            double nozzle_offset_from_b0_z = four_axis_Z_offset*(1- std::cos(b))
+            double nozzle_offset_from_b0_x = four_axis_Z_offset* std::sin(b)
             
             point.z = scale_(this->layer()->print_z) + nozzle_offset_from_b0_z;
             point.x = point.x + nozzle_offset_from_b0_z;
